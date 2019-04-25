bda9ca003c786231d1a8084325b94949efe6911e
Zane Hannan <zanehannanau@gmail.com>
2019-04-25 14:53:25 +1000

    Update parse to use better checks

--- src/parse.ts
+++ src/parse.ts
@@ -1,15 +1,15 @@
 import { tokenise } from "./tokenise";
-import { PACKABLE_TYPES, TokenCount, on_syntax_version, on_package_name, on_enum, on_message, on_option, on_import, on_extend, on_service, Lookup, LookupIn, Enum, Message, Extends } from "./parser-internals";
+import { PACKABLE_TYPES, TokenCount, on_syntax_version, on_package_name, on_enum, on_message, on_option, on_import, on_extend, on_service, Lookup, Enum, Message, Extends } from "./parser-internals";
 import { Schema } from "./schema";
 export { Schema } from "./schema";
 const exported_lookups = new WeakMap<Schema, Lookup>();
-export function find_lookup(s: Schema, name: string, is: "enum"): Enum
-export function find_lookup(s: Schema, name: string, is: "message"):Message
-export function find_lookup(s: Schema, name: string, is: "extends"): Extends
+export function find_lookup(s: Schema, name: string, is: "enum"): Enum | undefined
+export function find_lookup(s: Schema, name: string, is: "message"): Message | undefined
+export function find_lookup(s: Schema, name: string, is: "extends"): Extends | undefined
 export function find_lookup(schema: Schema, fname: string, fis: "enum" | "message" | "extends") {
-	if (!schema.package) throw new SyntaxError(`cannot take schemas without a package name`)
 	let v = exported_lookups.get(schema)!
-	.find(({is, name}) => is === fis && fname === name)!
+	.find(({is, name}) => is === fis && fname === name)
+	if (!v) return undefined
 	switch (fis) {
 		case "enum": return v.value as Enum;
 		case "message": return v.value as Message;
@@ -20,7 +20,7 @@ interface ToString {toString(): string;}
 export function parse<T extends ToString>(from: T) {
 	const schema = new Schema
 	const lu: Lookup = []
-	const exported: Lookup = [];
+	exported_lookups.set(schema, lu)
 	schema_parse: {
 		const tc = new TokenCount(Object.freeze(tokenise(from.toString())))
 		while (!tc.done) switch (tc.peek()) {
@@ -57,18 +57,16 @@ export function parse<T extends ToString>(from: T) {
 	}
 	schema_pack: for (const msg of schema.messages) for (const field of msg.fields) if (field.packed && !PACKABLE_TYPES.includes(field.type)) {
 		// check enum type
-		type LUE = LookupIn<'enum'> | undefined;
-		let type = lu.find(f => f.name === field.type && f.is === 'enum') as LUE
+		let type = find_lookup(schema, field.type, "enum")
 		if (type) continue
 		else if (field.type.includes('.')) {
 			const types = field.type.split('.')
-			let last = types.pop()
-			const mfind = (f: typeof lu[0]) => f.name === last && f.is === 'enum'
-			let curr = lu.find(mfind) as LUE
+			let last = types.pop()!
+			let curr = find_lookup(schema, last, "enum")
 			if (curr) while (types.length && curr) {
 				last = `${types.pop()!}.${last}`
-				let c = lu.find(mfind) as LUE
-				if (c && curr.value === c.value) curr = c
+				let c = find_lookup(schema, last, "enum")
+				if (c && curr !== c) curr = c
 			}
 
 			if (!curr)
@@ -82,11 +80,13 @@ export function parse<T extends ToString>(from: T) {
 		throw new SyntaxError(`Fields of type ${field.type} cannot be declared [packed=true]. Only repeated fields of primitive numeric types (types which use the varint, 32-bit, or 64-bit wire types) can be declared as "packed". See https://developers.google.com/protocol-buffers/docs/encoding#optional`)
 	}
 	if (schema.package) {
-		for (const {name, is, value} of lu) exported.push({
-			name: `${schema.package}.${name}`,
-			is, value
-		})
-		exported_lookups.set(schema, exported)
+		const exp: Lookup = []
+		for (const {is, value, name} of lu) if (
+			schema.enums.includes(value as Enum)
+			|| schema.messages.includes(value as Message)
+			|| schema.extends.includes(value as Extends)
+			) exp.push({ is, value, name: `${schema.package}.${name}` })
+		lu.push(...exp)
 	}
 	return schema
 }
