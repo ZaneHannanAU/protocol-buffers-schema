491daf39aec0ea48acb4b5f6640ad8563ea40ca1
Zane Hannan <zanehannanau@gmail.com>
2019-04-25 15:05:59 +1000

    build-test

--- lib/parse.d.ts
+++ lib/parse.d.ts
@@ -1,9 +1,9 @@
 import { Enum, Message, Extends } from "./parser-internals";
 import { Schema } from "./schema";
 export { Schema } from "./schema";
-export declare function find_lookup(s: Schema, name: string, is: "enum"): Enum;
-export declare function find_lookup(s: Schema, name: string, is: "message"): Message;
-export declare function find_lookup(s: Schema, name: string, is: "extends"): Extends;
+export declare function find_lookup(s: Schema, name: string, is: "enum"): Enum | undefined;
+export declare function find_lookup(s: Schema, name: string, is: "message"): Message | undefined;
+export declare function find_lookup(s: Schema, name: string, is: "extends"): Extends | undefined;
 interface ToString {
     toString(): string;
 }
