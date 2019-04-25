717ca04ed6eb1e908db621a0c19896113fcb0925
Zane Hannan <zanehannanau@gmail.com>
2019-04-25 14:54:37 +1000

    Run tests on ESM loader, better tests.

--- src/test.ts
+++ src/test.ts
@@ -1,48 +1,57 @@
-//@ts-nocheck
 import { parse } from "./parse";
-import {promises as fs} from 'fs';
+// shim until promises becomes an export of fs
+import { readdirSync as read_dir, readFileSync as read_file} from 'fs';
 import { join, basename } from 'path';
 import { Schema } from "./schema";
-import {deepStrictEqual as deep_strict_equal} from 'assert'
-console.log(fs)
+import {deepStrictEqual as deep_strict_equal, rejects} from 'assert'
 
-
-const fixture = (p: string) => join(__dirname, '../test/fixtures', basename(p))
-const rjson = (p: string) => fs.readFile(p, 'utf-8').then(JSON.parse)
-const rpbuf = (p: string) => fs.readFile(p, 'utf-8').then(parse)
+const fixture = (p: string) => join('test/fixtures', basename(p))
+const rjson = async (p: string) => JSON.parse(await read_file(p, 'utf-8'))
+const rpbuf = async (p: string) => parse(await read_file(p, 'utf-8'))
 //@ts-ignore
-const fixtures = (p: string) => {
+const fixtures = async (p: string) => {
 	const pf = fixture(p)
-	return Promise.all([rjson(pf + '.json'), rpbuf(pf + '.proto')])
+	const json = rjson(pf + '.json')
+	const schema = rpbuf(pf + '.proto')
+	return {json: await json, schema: await schema}
 }
-const will_error = new Set([
-	'no-tags',
-	'pheromon-trajectories'
-])
+const will_error: {[s: string]: Error} = {
+	'no-tags': {
+		name: "SyntaxError",
+		message: "Missing tag number in message field: dollars"
+	},
+	'pheromon-trajectories': {
+		name: "SyntaxError",
+		message: "Fields of type bytes cannot be declared \[packed=true\]. Only repeated fields of primitive numeric types (types which use the varint, 32-bit, or 64-bit wire types) can be declared as \"packed\". See https:\/\/developers.google.com\/protocol-buffers\/docs\/encoding\#optional"
+	}
+}
 async function tests() {
-	let dir = await fs.readdir(join(__dirname, '../test/fixtures'))
+	let dir = await read_dir('test/fixtures')
 
 	for (const file of new Set(dir.map(v => basename(basename(v, '.json'), '.proto')))) {
 		console.group(file)
-		if (will_error.has(file)) {
-			let err: Error | null = null, rp: Schema | null = null;
-			try { rp = await rpbuf(fixture(file + '.proto')) }
-			catch (e) {err = e}
-			if (err) console.error(err)
+		if (file in will_error) {
+			let err: Error | null = null, rp: Promise<Schema> | null = null;
+			await rejects(rp = rpbuf(fixture(file + '.proto')), will_error[file])
+			try { await rp } catch (e) {err = e}
+			if (err) console.log('Got error correctly: %s', err)
 			else {
 				console.error(rp)
 				throw new ReferenceError('Expected error, got schema instead.')
 			}
 		} else {
-			const [j, p] = await fixtures(file)
-			let pj = p.toJSON()
-			console.dir(p, {depth: null})
-			console.dir(j, {depth: null})
-			deep_strict_equal(j, pj, `${file} must be equal to its JSON value.`)
-			console.log(p.toString())
+			const {json, schema} = await fixtures(file)
+			let pj = schema.toJSON()
+			console.dir(schema, {depth: null})
+			console.dir(json, {depth: null})
+			deep_strict_equal(json, pj, `${file} must be equal to its JSON value.`)
+			console.log(schema.toString())
 		}
 		console.groupEnd()
 	}
 }
 
-tests().catch(console.error).finally(console.groupEnd)
+tests().catch(e => {
+	console.error(e)
+	process.exit(1)
+}).finally(console.groupEnd)
